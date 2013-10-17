9dc33d8fc9c186f11d3b751eca32f535c56b6449
dan jones <danwjones22@gmail.com>
2013-10-17 00:34:54 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rickjonespiano2::Application.config.secret_key_base = '223f01728bb29cf373f0c7d0cc28fb04d0ae9a2f477da1eee1557413a0d0e9068b07366632c6854382af235305a2aca6f750c65ccfb9a004dcccd49d1cf3cf5c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rickjonespiano2::Application.config.session_store :cookie_store, key: '_rickjonespiano2_session'
